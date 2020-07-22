ca514e2c26f50eb0259fd2f35c199c1bec0cd947
Muhammad Syihabuddin Az Zuhair <15320135+msazzuhair@users.noreply.github.com>
2020-07-22 12:48:56 +0700

    Restrict migration for non-admin

--- application/modules/migration/controllers/Migrate.php
+++ application/modules/migration/controllers/Migrate.php
@@ -2,6 +2,19 @@
 
 class Migrate extends MX_Controller
 {
+    public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->database();
+        $this->load->model('barang_model');
+
+		// Set user data
+		if (!$this->ion_auth->logged_in() || !$this->ion_auth->in_group('admin'))
+        {
+            show_404();
+        }
+	}
 
     public function index()
     {
